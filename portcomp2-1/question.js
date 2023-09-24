8f3e93ae655685a3de231370c6712c8f474eadd4
Hardik Singh Bhadoria <129764503+Hardik0612@users.noreply.github.com>
2023-09-24 23:02:10 +0530

    Add files via upload

---
+++ portcomp2-1/question.js
@@ -0,0 +1,48 @@
+let questions = [
+    {
+    numb: 1,
+    question: "JUMPING JACK",
+    answer: "DONE",
+    options: [
+      "SKIP",
+      "DONE",
+    ]
+  },
+    {
+    numb: 2,
+    question: " LUNGES ",
+    answer: "DONE",
+    options: [
+      "SKIP",
+      "DONE",
+    ]
+  },
+    {
+    numb: 3,
+    question: "WIDE ARM PUSHUP",
+    answer: "DONE",
+    options: [
+      "SKIP",
+      "DONE",
+    ]
+  },
+    {
+    numb: 4,
+    question: "COBRA STRETCH",
+    answer: "DONE",
+    options: [
+      "SKIP",
+      "DONE",
+    ]
+  },
+    {
+    numb: 5,
+    question: "CHEST STRETCH",
+    answer: "DONE",
+    options: [
+      "SKIP",
+      "DONE",
+     
+    ]
+  },
+];
